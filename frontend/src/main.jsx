a77b03d722f5fbc287255a652d8d4d680127aa69
Biloliddin-Tursunov <tursunovbiloliddin2006@gmail.com>
2025-10-05 02:05:10 +0500

    add Web Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/style.css";
 import "./styles/fonts.css";
 import { injectSpeedInsights } from "@vercel/speed-insights";
+import { Analytics } from "@vercel/analytics/next";
+
 if (import.meta.env.PROD) injectSpeedInsights();
+if (import.meta.env.PROD) Analytics();
 
 import App from "./App.jsx";
 
