4928d1883badec873a703b4d0ed78bbb82b24a41
Biloliddin-Tursunov <tursunovbiloliddin2006@gmail.com>
2025-10-20 00:50:59 +0500

    added bg music

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import Hero from "../components/Hero";
 import Links from "../components/Links";
+import MusicPlayer from "../components/MusicPlayer";
 import TeamSection from "../components/TeamSection";
 
 export default function Home() {
     return (
-        <div className="main">
-            <Hero />
-            <Links />
-            <TeamSection />
+        <div>
+            <div className="fixed left-6 bottom-6 z-50">
+                <MusicPlayer />
+            </div>
+
+            <main className="main">
+                <Hero />
+                <Links />
+                <TeamSection />
+            </main>
         </div>
     );
 }
