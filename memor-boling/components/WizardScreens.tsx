b4cf155cdfda7bf1b9306872b55caf7f39030247
Biloliddin-Tursunov <tursunovbiloliddin2006@gmail.com>
2026-01-22 14:12:36 +0500

    remove

--- memor-boling/components/WizardScreens.tsx
+++
@@ -1,3 +0,0 @@
-// This file is deprecated. Components have been moved to /components/steps/
-// Keeping this file empty to avoid import errors if referenced elsewhere during transition.
-export {};
